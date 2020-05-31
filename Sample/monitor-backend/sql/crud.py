93aaca3f92b34e2252cd2345da9b7fe0b4a65cc0
Sandeep <sandeep.p@Sandeepp.local>
2020-05-31 16:12:24 +0530

    Added Floor and Ward number as path param in beds api

--- Sample/monitor-backend/sql/crud.py
+++ Sample/monitor-backend/sql/crud.py
@@ -4,29 +4,31 @@ from . import models, schemas
 from .models import Patient, MedicalDetails, BedDetails
 
 
-def get_patient_by_id(db: Session, id: str):
-    return db.query(Patient, BedDetails, MedicalDetails).join(BedDetails,
+def get_bed_details_by_id(db: Session, bed_id: str):
+    return db.query(BedDetails, Patient, MedicalDetails).join(Patient,
                                                               Patient.patient_id == BedDetails.current_patient_id).join(
-        MedicalDetails, Patient.patient_id == MedicalDetails.patient_id).filter(Patient.patient_id == id).first()
+        MedicalDetails, BedDetails.current_patient_id == MedicalDetails.patient_id).filter(
+        BedDetails.bed_id == bed_id).first()
 
 
-def create_user(db: Session, patient: schemas.Patient):
-    db_user = models.Patient(patient_id=patient.patient_id, name=patient.name, sex=patient.sex, age=patient.age)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+# def create_user(db: Session, patient: schemas.Patient):
+#     db_user = models.Patient(patient_id=patient.patient_id, name=patient.name, sex=patient.sex, age=patient.age)
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
+#     return db_user
 
 
-def get_all_bed_details(db: Session):
+def get_all_bed_details(db: Session, ward_number: str, floor_number: str):
     bed_details = db.query(BedDetails, Patient, MedicalDetails).join(Patient,
-                                                                          Patient.patient_id == BedDetails.current_patient_id).join(
-        MedicalDetails, BedDetails.current_patient_id == MedicalDetails.patient_id).all()
+                                                                     Patient.patient_id == BedDetails.current_patient_id).join(
+        MedicalDetails, BedDetails.current_patient_id == MedicalDetails.patient_id).filter(
+        BedDetails.ward_no == ward_number).filter(BedDetails.floor_number == floor_number).all()
     return bed_details
 
 
 def delete_patient_by_id(db: Session, id: str):
-    by_id = get_patient_by_id(db, id)
+    by_id = get_bed_details_by_id(db, id)
     try:
         db.delete(by_id)
         db.commit()
