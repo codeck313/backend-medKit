66afd4c8275ec532602465ad44d89a04e4113c23
Parikshit Iyengar <2667254+piyengar@users.noreply.github.com>
2020-06-01 16:38:47 +0530

    fixed variable names in bed response

--- Sample/monitor-backend/sql/response.py
+++ Sample/monitor-backend/sql/response.py
@@ -29,28 +29,33 @@ class BedResponse:
     spO2Avg: float
 
     def __init__(self, bed_details: BedDetails, patient_details: Patient, medical_details: MedicalDetails):
-        self.id = patient_details.patient_id
-        self.name = patient_details.name
-        self.age = patient_details.age
-        self.sex = patient_details.sex
-        self.floorNumber = bed_details.floor_number
-        self.bedNo = str(bed_details.bed_no)
-        self.ip_address = bed_details.ip_address
-        self.ward_no = bed_details.ward_no
-        self.bed_id = str(bed_details.bed_id)
-        self.time = str(medical_details.time)
-        self.heartRateMaxima = patient_details.heart_rate_maxima
-        self.heartRateMinima = patient_details.heart_rate_minima
-        self.spO2Minima = patient_details.spo2_minima
-        self.diastolicBPMaxima = patient_details.diastolic_bp_maxima
-        self.systolicBPMaxima = patient_details.systolic_bp_maxima
-        self.tempCurrent = medical_details.temp_current
-        self.tempAvg = medical_details.temp_avg
-        self.bpmCurrent = medical_details.bpm_current
-        self.bpmAvg = medical_details.bpm_avg
-        self.bpSystolicCurrent = medical_details.bp_systolic_current
-        self.bpSystolicAvg = medical_details.bp_systolic_avg
-        self.bpDiastolicCurrent = medical_details.bp_diastolic_current
-        self.bpDiastolicAvg = medical_details.bp_diastolic_avg
-        self.spO2Current = medical_details.spo2_current
-        self.spO2Avg = medical_details.spo2_avg
+        if patient_details is not None:
+            self.id = patient_details.patient_id
+            self.name = patient_details.name
+            self.age = patient_details.age
+            self.sex = patient_details.sex
+            self.heartRateMaxima = patient_details.heart_rate_maxima
+            self.heartRateMinima = patient_details.heart_rate_minima
+            self.spO2Minima = patient_details.spo2_minima
+            self.diastolicBPMaxima = patient_details.diastolic_bp_maxima
+            self.systolicBPMaxima = patient_details.systolic_bp_maxima
+
+        if bed_details is not None:
+            self.floorNumber = bed_details.floor_number
+            self.bedNo = str(bed_details.bed_no)
+            self.ipAddress = bed_details.ip_address
+            self.wardNo = bed_details.ward_no
+            self.bedId = str(bed_details.bed_id)
+
+        if medical_details is not None:
+            self.time = str(medical_details.time)
+            self.tempCurrent = medical_details.temp_current
+            self.tempAvg = medical_details.temp_avg
+            self.bpmCurrent = medical_details.bpm_current
+            self.bpmAvg = medical_details.bpm_avg
+            self.bpSystolicCurrent = medical_details.bp_systolic_current
+            self.bpSystolicAvg = medical_details.bp_systolic_avg
+            self.bpDiastolicCurrent = medical_details.bp_diastolic_current
+            self.bpDiastolicAvg = medical_details.bp_diastolic_avg
+            self.spO2Current = medical_details.spo2_current
+            self.spO2Avg = medical_details.spo2_avg
